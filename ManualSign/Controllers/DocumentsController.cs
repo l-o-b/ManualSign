bf8f9bda9c3f844c4063ea32eae03bac55983699
l-o-b <l-o-b@list.ru>
2016-04-20 12:06:02 +1000

    initial commit
    Добавлены файлы проекта

---
+++ ManualSign/Controllers/DocumentsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ManualSign.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        // GET: Documents
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
